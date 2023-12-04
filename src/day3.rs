58f45c85d11987ee0cfcee42534e96aecfab94c0
Freyja Østrem <1686349+frxstrem@users.noreply.github.com>
2023-12-04 02:38:31 +0100

    2023 day 3: cleanup

--- src/day3.rs
+++ src/day3.rs
@@ -2,8 +2,32 @@ use aoc_runner_derive::{aoc, aoc_generator};
 
 #[derive(Clone, Debug, Default)]
 struct Schematic {
-    numbers: Vec<(usize, usize, usize, u32)>,
-    symbols: Vec<(usize, usize, char)>,
+    numbers: Vec<Number>,
+    symbols: Vec<Symbol>,
+}
+
+#[derive(Clone, Debug)]
+struct Number {
+    i: usize,
+    j: usize,
+    l: usize,
+    n: u32,
+}
+
+impl Number {
+    fn is_adjacent_to(&self, symbol: &Symbol) -> bool {
+        symbol.i + 1 >= self.i
+            && symbol.i <= self.i + 1
+            && symbol.j + 1 >= self.j
+            && symbol.j <= self.j + self.l
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Symbol {
+    i: usize,
+    j: usize,
+    s: char,
 }
 
 #[aoc_generator(day3)]
@@ -18,15 +42,18 @@ fn parse_input(input: &str) -> Schematic {
                     line.find(|ch: char| !ch.is_ascii_digit())
                         .unwrap_or(line.len()),
                 );
-                schematic
-                    .numbers
-                    .push((i, j, num.len(), num.parse::<u32>().unwrap()));
+                schematic.numbers.push(Number {
+                    i,
+                    j,
+                    l: num.len(),
+                    n: num.parse::<u32>().unwrap(),
+                });
                 line = rest;
                 j += num.len();
             } else if line.starts_with(|ch: char| ch != '.') {
                 let mut c = line.chars();
-                let symbol = c.next().unwrap();
-                schematic.symbols.push((i, j, symbol));
+                let s = c.next().unwrap();
+                schematic.symbols.push(Symbol { i, j, s });
                 line = c.as_str();
                 j += 1;
             }
@@ -49,13 +76,8 @@ fn part1(input: &Schematic) -> u32 {
     input
         .numbers
         .iter()
-        .filter(|(i, j, l, _)| {
-            input
-                .symbols
-                .iter()
-                .any(|(si, sj, _)| *si + 1 >= *i && *si <= *i + 1 && *sj + 1 >= *j && *sj <= *j + l)
-        })
-        .map(|(_, _, _, n)| n)
+        .filter(|num| input.symbols.iter().any(|sym| num.is_adjacent_to(sym)))
+        .map(|num| num.n)
         .sum()
 }
 
@@ -64,14 +86,12 @@ fn part2(input: &Schematic) -> u32 {
     input
         .symbols
         .iter()
-        .filter(|(_, _, s)| *s == '*')
-        .filter_map(|(si, sj, _)| {
-            let mut part_numbers = input.numbers.iter().filter(|(i, j, l, _)| {
-                *si + 1 >= *i && *si <= *i + 1 && *sj + 1 >= *j && *sj <= *j + l
-            });
+        .filter(|sym| sym.s == '*')
+        .filter_map(|sym| {
+            let mut part_numbers = input.numbers.iter().filter(|num| num.is_adjacent_to(sym));
 
-            let pn1 = part_numbers.next()?.3;
-            let pn2 = part_numbers.next()?.3;
+            let pn1 = part_numbers.next()?.n;
+            let pn2 = part_numbers.next()?.n;
             if part_numbers.next().is_some() {
                 return None;
             }
